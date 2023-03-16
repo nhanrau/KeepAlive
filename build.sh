0a9959014a24e4c27ad6516b6816271fd55d0bc8
nhanrau <92256300+nhanrau@users.noreply.github.com>
2023-03-16 08:41:17 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 . /etc/profile
 
 # Linux arm64
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -trimpath -ldflags="-s -w -buildid=" -o NeverIdle-linux-arm64 main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -trimpath -ldflags="-s -w -buildid=" -o gotest-linux-arm64 main.go
 # Linux amd64
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-s -w -buildid=" -o NeverIdle-linux-amd64 main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-s -w -buildid=" -o gotest-linux-amd64 main.go
 # Windows amd64
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -trimpath -ldflags="-s -w -buildid=" -o NeverIdle-windows-amd64 main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -trimpath -ldflags="-s -w -buildid=" -o gotest-windows-amd64 main.go
 # macOS amd64
-#CGO_ENABLED=0 GOOS=linux GOARCH=darwin go build -trimpath -ldflags="-s -w -buildid=" -o NeverIdle-darwin-amd64 main.go
+#CGO_ENABLED=0 GOOS=linux GOARCH=darwin go build -trimpath -ldflags="-s -w -buildid=" -o gotest-darwin-amd64 main.go
